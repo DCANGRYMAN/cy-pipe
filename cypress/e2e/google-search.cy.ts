49d6eebd9a4fc2cfd346e8af5a2475a722590927
Darlan <darlandc@gmail.com>
2024-07-16 03:11:33 -0300

    set run command

--- cypress/e2e/google-search.cy.ts
+++ cypress/e2e/google-search.cy.ts
@@ -1,7 +1,5 @@
 describe('Google Search', () => {
   it('should search for Cypress', () => {
     cy.visit('https://www.google.com')
-    cy.get('input[name="q"]').type('Cypress.io{enter}')
-    cy.contains('Cypress.io').should('exist')
   })
 })
