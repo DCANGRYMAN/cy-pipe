e20ed441b68ac43568a2f6e03b94b63b6ed26e25
Darlan <darlandc@gmail.com>
2024-07-16 03:19:13 -0300

    start ok

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,17 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:3000', // Ajuste para a URL base do seu projeto
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Implemente os ouvintes de eventos do Node aqui, se necessário
     },
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}', // Padrão de localização dos seus arquivos de teste
+    supportFile: 'cypress/support/e2e.{js,ts}', // Localização do arquivo de suporte
+    // Ajustes de timeouts
+    pageLoadTimeout: 10000, // Tempo máximo em milissegundos para esperar o carregamento de uma página
+    defaultCommandTimeout: 10000, // Tempo máximo em milissegundos para comandos do Cypress serem concluídos
+    requestTimeout: 5000, // Tempo máximo em milissegundos para uma requisição do cy.request ser concluída
+    responseTimeout: 5000, // Tempo máximo em milissegundos para esperar uma resposta para um comando cy.visit(), cy.request(), cy.route() e cy.route2()
   },
 });
